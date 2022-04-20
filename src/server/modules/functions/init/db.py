34c7fd6ea64fec922fb0a94f0700cbbf6950172f
Brian <bcwilliams198@gmail.com>
2022-04-20 08:09:46 -0400

    DB causing errors; commenting out for now

--- src/server/modules/functions/init/db.py
+++ src/server/modules/functions/init/db.py
@@ -10,5 +10,5 @@ from ...data.models import db
 
 def init_db(app):
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
