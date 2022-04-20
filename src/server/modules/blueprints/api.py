1f5c862676f986305e0b803d2702503a87836be6
Brian <bcwilliams198@gmail.com>
2022-04-20 07:21:27 -0400

    Committing edits to blueprints

--- src/server/modules/blueprints/api.py
+++ src/server/modules/blueprints/api.py
@@ -1,10 +1,24 @@
-# pylint: disable =
+# pylint: disable = missing-function-docstring, relative-beyond-top-level
 """
 Disabled Pylint Warnings & Justifications:
+missing-function-docstring: useful, but not necessary; takes up space
 """
 
+
+### IMPORTS
+# third-party
 from flask import Blueprint
 
+# native
+from ..functions.external_apis.pokeapi import get_pokemon_data
+
+
 api_blueprint = Blueprint("api", __name__, url_prefix="/api")
+
+
+@api_blueprint.route("/search_pokemon")
+def search_pokemon(pokemon):
+    return get_pokemon_data(pokemon.lower())
+
 
 # all database/API-calling routes belong here
