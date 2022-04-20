7a45e591d313e36ce80b7d422da85cbf3c6f530a
Brian <bcwilliams198@gmail.com>
2022-04-20 06:48:20 -0400

    Porting over login code

--- src/server/modules/blueprints/login.py
+++ src/server/modules/blueprints/login.py
@@ -1,17 +1,23 @@
-# pylint: disable = missing-function-docstring
+# pylint: disable = missing-function-docstring, relative-beyond-top-level
 """
 Disabled Pylint Warnings & Justifications:
 missing-function-docstring: useful, but not necessary; takes up space
+relative-beyond-top-level: pylint doesn't seem to like relative imports
 """
 
 
 ### IMPORTS
 # third-party
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
 from flask.helpers import send_from_directory
 
 # from flask_login import login_user, logout_user
 
+# native
+from ..functions.external_apis.google import get_client, get_google_provider_cfg
+
+
+client = get_client()
 
 login_blueprint = Blueprint("login", __name__, static_folder="../../../client/build")
 
@@ -24,3 +30,17 @@ def login():
 @login_blueprint.route("/logout")
 def logout():
     pass
+
+
+@login_blueprint.route("/login_request")
+def login_request():
+    google_provider_cfg, error = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+    # use library to construct the request for login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=f"{request.base_url}/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return jsonify(request_uri)
